858a55f02775b2ef32dab28e1c272e4c7974bc53
Hasibur Rahman <hasib.active@gmail.com>
2022-02-08 15:01:09 +0600

    updated

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -3,27 +3,23 @@ import env from './env'
 
 // Swagger
 const options = {
-   definition: {
-      openapi: '3.0.0',
-      info: {
-         title: 'Api Documentation',
-         description: 'This is a api Documentation for my application.',
-         version: '1.0.0',
-         contact: {
-            email: 'hasib.webdev@gmail.com'
+   "definition": {
+      "openapi": "3.0.0",
+      "info": {
+         "title": "Api Documentation",
+         "description": 'This is a api Documentation for my application.',
+         "version": "1.0.0",
+         "contact": {
+            "email": "hasib.webdev@gmail.com"
          },
-         liscence: {
-            name: 'Apache 2.0',
-            url: 'http://apache.org/'
-         }
       },
-      servers: [
-         { url: `http://localhost:${env.port}`, description: 'local server' },
-         { url: 'http://example.com', description: 'Production server' },
+      "servers": [
+         { "url": `http://localhost:${env.port}`, "description": "local server" },
+         { "url": 'http://example.com', "description": "Production server" },
       ],
 
    },
-   apis: ['src/apidocs/*.yaml'], // files containing annotations as above
+   "apis": ["apidocs/*.yaml"], // files containing annotations as above
 }
 
 const swaggerSpecs = swaggerJsdoc(options)
